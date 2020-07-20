604d7875a4ac80f25056ede0dcf3a5430cd73521
Roger-luo <rogerluo.rl18@gmail.com>
2020-07-20 02:45:09 -0400

    dev Comonicon

--- deps/build.jl
+++ deps/build.jl
@@ -2,6 +2,6 @@ using Pkg
 PROJECT = joinpath(dirname(dirname(@__FILE__)), "example", "Ion")
 Pkg.activate(PROJECT)
 Pkg.instantiate()
-
+Pkg.develop("../..")
 using Comonicon, Ion
 Comonicon.install(Ion, "ion"; compile=:min)
