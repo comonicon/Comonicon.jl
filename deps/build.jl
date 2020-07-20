592b253d18ea01f04766fa730f74c20c32470350
Roger-luo <rogerluo.rl18@gmail.com>
2020-07-20 02:43:52 -0400

    instanitiate env

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,7 @@
 using Pkg
 PROJECT = joinpath(dirname(dirname(@__FILE__)), "example", "Ion")
 Pkg.activate(PROJECT)
+Pkg.instantiate()
 
 using Comonicon, Ion
 Comonicon.install(Ion, "ion"; compile=:min)
