92f670185e377d3432306c5d01640900a25a4318
Roger-luo <rogerluo.rl18@gmail.com>
2020-07-29 01:59:21 -0400

    allow changing width by terminal_width, default is 85 now

--- src/types.jl
+++ src/types.jl
@@ -254,7 +254,7 @@ function print_version(io::IO)
     print(io, " "^doc_indent, VERSION_FLAG_DOC, "\n\n")
 end
 
-function partition(io, cmd, xs...; width = 80)
+function partition(io, cmd, xs...; width = get(io, :terminal_width, 85))
     doc_indent = get(io, :doc_indent, -1)
     doc_width = width - doc_indent
     first_line_indent = first_line_doc_indent(io, cmd)
@@ -307,7 +307,7 @@ function splittext(s)
     return result
 end
 
-function splitlines(s, width = 120)
+function splitlines(s, width = 80)
     words = splittext(s)
     lines = String[]
     current_line = String[]
