88fe98cb8106227110ee28bcf625b582132e0356
Micah Halter <micah.halter@gtri.gatech.edu>
2020-08-01 14:45:51 -0400

    Fix zsh config location to respect ZSH environment variables

--- src/tools/build.jl
+++ src/tools/build.jl
@@ -551,9 +551,9 @@ function install_env_path(quiet::Bool=false)
 
     config_file = ""
     if shell == "zsh"
-        config_file = ".zshrc"
+        config_file = joinpath((haskey(ENV, "ZDOTDIR") ? ENV["ZDOTDIR"] : homedir()), ".zshrc")
     elseif shell == "bash"
-        config_file = ".bashrc"
+        config_file = joinpath(homedir(), ".bashrc")
     else
         @warn "auto installation for $shell is not supported, please open an issue under Comonicon.jl"
     end
