7f026783bfefbbcad47c99301fffa22f6cdd4eef
Roger-luo <rogerluo.rl18@gmail.com>
2020-10-09 16:06:13 -0400

    rm old examples

--- example/Ion/src/precompile.jl
+++
@@ -1,4 +0,0 @@
-function _precompile_()
-    ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
-    Base.precompile(Tuple{typeof(command_main),Array{String,1}})
-end
--- example/hello/bin/comonicon.jl
+++
@@ -1,2 +0,0 @@
-using Hello;
-Hello.command_main();
