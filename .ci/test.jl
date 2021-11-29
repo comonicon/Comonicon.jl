aa7c7537adf64a6dff686c82a69f3dba06aa825e
Roger-luo <rogerluo.rl18@gmail.com>
2021-11-29 01:21:30 -0500

    fix ci

--- .ci/test.jl
+++ .ci/test.jl
@@ -1,57 +1,47 @@
-root = dirname(@__DIR__)
-lib_dir = joinpath(root, "lib")
-example_dir = joinpath(root, "example")
+using Pkg
+using TestEnv
+using Comonicon
 
-# activate root
-using Pkg; Pkg.activate(root)
-comonicon_jl = PackageSpec(path=root)
-# collect packages
-pkgs = []
-names = String[]
-
-if isempty(ARGS) # test all by default
-    push!(names, "Comonicon")
+@main function command(test_pkgs::String...; coverage::Bool=false)
+    root = dirname(@__DIR__)
+    lib_dir = joinpath(root, "lib")
+    example_dir = joinpath(root, "example")
+    # activate root
+    Pkg.activate(root)
+    comonicon_jl = PackageSpec(path=root)
+    # collect packages
+    pkgs = [comonicon_jl]
+    names = String[]
+    
+    # collect libraries
     for each_lib in readdir(lib_dir)
         path = joinpath(lib_dir, each_lib)
-        if isdir(path)
-            push!(pkgs, PackageSpec(;path))
-            push!(names, each_lib)
-        end
+        isdir(path) || continue
+        push!(pkgs, PackageSpec(;path))
+        push!(names, each_lib)
     end
-
+    
     for each_example in readdir(example_dir)
         path = joinpath(example_dir, each_example)
         isdir(path) || continue
+        # we need to generate Manifest.toml for examples
+        # to build sysimg and app
         package = PackageSpec(;path)
         Pkg.activate(path)
         Pkg.develop(comonicon_jl)
         push!(pkgs, package)
         push!(names, each_example)
     end
-    Pkg.activate(root)
-    push!(pkgs, comonicon_jl)
-else
-    for each in ARGS
-        if each == "Comonicon"
-            push!(pkgs, comonicon_jl)
-            push!(names, each)
+
+    TestEnv.activate() do
+        foreach(Pkg.develop, pkgs)
+        Pkg.status()
+        # start test
+        @show test_pkgs
+        if isempty(test_pkgs)
+            Pkg.test(names; coverage)
         else
-            path = joinpath(root, each)
-            isdir(path) || continue
-            package = PackageSpec(;path)
-            Pkg.activate(path)
-            Pkg.develop(comonicon_jl)
-            push!(pkgs, package)
-            push!(names, each)
+            Pkg.test(collect(test_pkgs); coverage)
         end
-    end
-end
-
-using TestEnv
-
-TestEnv.activate() do
-    foreach(Pkg.develop, pkgs)
-    Pkg.status()
-    # start test
-    Pkg.test(names; coverage=true)
+    end    
 end
