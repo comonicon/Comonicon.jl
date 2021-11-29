dca704f90816ce3d5d23ed5cae1ac9963a30e84b
Roger-luo <rogerluo.rl18@gmail.com>
2021-11-29 01:33:12 -0500

    fix tests

--- .ci/test.jl
+++ .ci/test.jl
@@ -2,25 +2,40 @@ using Pkg
 using TestEnv
 using Comonicon
 
-@main function command(test_pkgs::String...; coverage::Bool=false)
+"""
+run Comonicon tests.
+
+# Args
+
+- `testset`: which testset to run, can be `all`, `Comonicon`, `lib`, `example`.
+
+# Flags
+
+- `--coverage`: enable code coverage tracking.
+"""
+@main function command(testset::String="all"; coverage::Bool=false)
     root = dirname(@__DIR__)
     lib_dir = joinpath(root, "lib")
     example_dir = joinpath(root, "example")
     # activate root
     Pkg.activate(root)
     comonicon_jl = PackageSpec(path=root)
-    # collect packages
-    pkgs = [comonicon_jl]
-    names = String[]
     
+    # collect packages
+    lib_pkgs = []
+    lib_names = String[]
+    example_pkgs = []
+    example_names = String[]
+
     # collect libraries
     for each_lib in readdir(lib_dir)
         path = joinpath(lib_dir, each_lib)
         isdir(path) || continue
-        push!(pkgs, PackageSpec(;path))
-        push!(names, each_lib)
+        push!(lib_pkgs, PackageSpec(;path))
+        push!(lib_names, each_lib)
     end
     
+    # collect examples
     for each_example in readdir(example_dir)
         path = joinpath(example_dir, each_example)
         isdir(path) || continue
@@ -29,19 +44,25 @@ using Comonicon
         package = PackageSpec(;path)
         Pkg.activate(path)
         Pkg.develop(comonicon_jl)
-        push!(pkgs, package)
-        push!(names, each_example)
+        push!(example_pkgs, package)
+        push!(example_names, each_example)
     end
 
     TestEnv.activate() do
-        foreach(Pkg.develop, pkgs)
+        foreach(Pkg.develop, lib_pkgs)
+        foreach(Pkg.develop, example_pkgs)
         Pkg.status()
         # start test
-        @show test_pkgs
-        if isempty(test_pkgs)
-            Pkg.test(names; coverage)
-        else
-            Pkg.test(collect(test_pkgs); coverage)
+        if testset == "all"
+            Pkg.test("Comonicon"; coverage)
+            Pkg.test(lib_names; coverage)
+            Pkg.test(example_names; coverage)
+        elseif testset == "Comonicon"
+            Pkg.test("Comonicon"; coverage)
+        elseif testset == "example"
+            Pkg.test(example_names; coverage)
+        elseif testset == "lib"
+            Pkg.test(lib_names; coverage)
         end
     end    
 end
