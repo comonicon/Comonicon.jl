b001c36b36fdcd3c80b95a16b7e1e530cdeb7059
Roger-luo <rogerluo.rl18@gmail.com>
2021-11-29 01:55:42 -0500

    upgrade CI

--- .ci/test.jl
+++ .ci/test.jl
@@ -2,6 +2,68 @@ using Pkg
 using TestEnv
 using Comonicon
 
+function collect_lib()
+    root = dirname(@__DIR__)
+    lib_dir = joinpath(root, "lib")
+    lib_pkgs = []
+    lib_names = String[]
+    # collect libraries
+    for each_lib in readdir(lib_dir)
+        path = joinpath(lib_dir, each_lib)
+        isdir(path) || continue
+        push!(lib_pkgs, PackageSpec(;path))
+        push!(lib_names, each_lib)
+    end
+    return lib_pkgs, lib_names
+end
+
+function collect_example()
+    root = dirname(@__DIR__)
+    example_dir = joinpath(root, "example")
+    example_pkgs = []
+    example_names = String[]
+    # collect examples
+    for each_example in readdir(example_dir)
+        path = joinpath(example_dir, each_example)
+        isdir(path) || continue
+        push!(example_pkgs, PackageSpec(;path))
+        push!(example_names, each_example)
+    end
+    return example_pkgs, example_names
+end
+
+function generate_example_manifest(pkgs)
+    # we need to generate Manifest.toml for examples
+    # to build sysimg and app
+    root = dirname(@__DIR__)
+    comonicon_jl = PackageSpec(path=root)
+    for pkg in pkgs
+        Pkg.activate(pkg.path)
+        Pkg.develop(comonicon_jl)
+    end
+    return
+end
+
+"""
+develop package set at current activate environment.
+
+# Args
+
+- `set`: package set name, can be `Comonicon`, `lib`, `example`, `all`.
+"""
+@cast function dev(set::String="all")
+    root = dirname(@__DIR__)
+    if set == "Comonicon"
+        Pkg.develop(PackageSpec(path=root))
+    elseif set == "lib"
+        lib_pkgs, lib_names = collect_lib()
+        foreach(Pkg.develop, lib_pkgs)
+    elseif set == "example"
+        example_pkgs, example_names = collect_example()
+        foreach(Pkg.develop, example_pkgs)
+    end
+end
+
 """
 run Comonicon tests.
 
@@ -13,44 +75,18 @@ run Comonicon tests.
 
 - `--coverage`: enable code coverage tracking.
 """
-@main function command(testset::String="all"; coverage::Bool=false)
+@cast function runtest(testset::String="all"; coverage::Bool=false)
     root = dirname(@__DIR__)
-    lib_dir = joinpath(root, "lib")
-    example_dir = joinpath(root, "example")
-    # activate root
-    Pkg.activate(root)
     comonicon_jl = PackageSpec(path=root)
-    
-    # collect packages
-    lib_pkgs = []
-    lib_names = String[]
-    example_pkgs = []
-    example_names = String[]
 
-    # collect libraries
-    for each_lib in readdir(lib_dir)
-        path = joinpath(lib_dir, each_lib)
-        isdir(path) || continue
-        push!(lib_pkgs, PackageSpec(;path))
-        push!(lib_names, each_lib)
-    end
-    
-    # collect examples
-    for each_example in readdir(example_dir)
-        path = joinpath(example_dir, each_example)
-        isdir(path) || continue
-        # we need to generate Manifest.toml for examples
-        # to build sysimg and app
-        package = PackageSpec(;path)
-        Pkg.activate(path)
-        Pkg.develop(comonicon_jl)
-        push!(example_pkgs, package)
-        push!(example_names, each_example)
-    end
+    lib_pkgs, lib_names = collect_lib()
+    example_pkgs, example_names = collect_example()
 
     TestEnv.activate() do
+        Pkg.develop(comonicon_jl)
         foreach(Pkg.develop, lib_pkgs)
         foreach(Pkg.develop, example_pkgs)
+
         Pkg.status()
         # start test
         if testset == "all"
@@ -64,5 +100,7 @@ run Comonicon tests.
         elseif testset == "lib"
             Pkg.test(lib_names; coverage)
         end
-    end    
+    end
 end
+
+@main
