a730b422d42c3cb8eb67536d163156d17777ac54
Roger-Luo <rogerluo.rl18@gmail.com>
2022-02-02 00:11:21 -0500

    check if GHA is interactive

--- test/tools.jl
+++ test/tools.jl
@@ -1,6 +1,8 @@
 using Test
 using Comonicon
 using Comonicon.Tools: prompt
+using InteractiveUtils
+@info "interactive" isinteractive()
 
 @testset "prompt(msg) input=$yes" for yes in ['Y', 'y', '\n', '\r']
     print(stdin.buffer, yes)
