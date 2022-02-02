9fde86453bc854830277f5edded6121407e0d87a
Roger-Luo <rogerluo.rl18@gmail.com>
2022-02-01 20:30:46 -0500

    allow custom prompt msg

--- src/tools.jl
+++ src/tools.jl
@@ -1,10 +1,36 @@
 module Tools
 
-prompt(msg, yes::Bool = false) = prompt(stdin, msg, yes)
+"""
+    prompt(msg::AbstractString[, yes::Bool=false])
 
-function prompt(io::IO, msg, yes::Bool = false)
-    print(msg)
+Prompt with message to ask user Yes or No.
 
+# Arguments
+
+- `msg`: the message to show in prompt.
+- `yes`: skip the prompt and always return `true`, default is `false`.
+"""
+function prompt(msg::AbstractString, yes::Bool = false)
+    prompt(yes) do
+        print(msg)
+    end
+end
+
+"""
+    prompt(f, [io=stdin,] yes::Bool=false)
+
+Prompt with custom printing to ask user Yes or No.
+
+# Arguments
+
+- `f`: a function with no arguments, which prints the prompt message.
+- `io`: user input stream, default is `stdin`.
+- `yes`: skip the prompt and always return `true`, default is `false`.
+"""
+prompt(f, yes::Bool=false) = prompt(f, stdin, yes)
+
+function prompt(f, io::IO, yes::Bool = false)
+    f() # print message
     if yes
         println(" Yes.")
     else
