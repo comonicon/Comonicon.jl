8a5aecad93102d1680d959d202fa0bcbc42859b5
Rogerluo <rogerluo.rl18@gmail.com>
2022-02-10 23:51:10 -0500

    fix long sig printing (#176)
    * fix long sig printing
    
    * fix entry doc printing

--- src/frontend/cast.jl
+++ src/frontend/cast.jl
@@ -370,7 +370,7 @@ function codegen_create_entry(m::Module, line, @nospecialize(ex))
     @gensym cmd entry
     configs = Configs.read_options(m)
     julia_expr_configs =
-        JuliaExpr.Configs(; configs.command.color, configs.command.static, configs.command.dash)
+        JuliaExpr.Configs(; configs.command.width, configs.command.color, configs.command.static, configs.command.dash)
     quote
         $(codegen_entry_cmd(m::Module, line, cmd, configs, ex))
         $entry = $Comonicon.AST.Entry($cmd, $(get_version(m)), $line)
