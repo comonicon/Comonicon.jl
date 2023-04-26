b3529763d336b607213ac543cd90a0b9ee1f3e51
Xiu-zhe (Roger) Luo <rogerluo.rl18@gmail.com>
2023-04-26 14:21:15 -0400

    use JULIA_DEPOT_PATH as default install path (#240)
    * use JULIA_DEPOT_PATH as default install path
    
    * change to DEPOT_PATH

--- src/configs.jl
+++ src/configs.jl
@@ -63,7 +63,7 @@ Installation configurations.
 - `nthreads`: julia compiler option for CLIs if not built as standalone application, default is `1`.
 """
 @option struct Install
-    path::String = "~/.julia"
+    path::String = first(DEPOT_PATH)
     completion::Bool = true
     quiet::Bool = false
     compile::String = "yes"
