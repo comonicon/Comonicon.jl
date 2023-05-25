b52cd6b1b6ce69829a2ee5f35d29a4b2643c9c6a
Johnny Chen <johnnychen94@hotmail.com>
2023-05-25 14:41:28 +0800

    use oneliner version

--- src/builder/install.jl
+++ src/builder/install.jl
@@ -163,9 +163,7 @@ function _entryfile_script_bat(m::Module, options::Configs.Comonicon)
     setlocal
     set JULIA_PROJECT=$(get_scratch!(m, "env"))
     $(join(cmds, " ^\n    "))
-    IF %ERRORLEVEL% NEQ 0 (
-        exit /b %ERRORLEVEL%
-    )
+    if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
     endlocal
     """
 end
